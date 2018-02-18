de66c08d8cd10e60a486aed005fba766d0db0dcb
lucasmaryn <lucasmaryn@interia.pl>
2018-02-18 18:20:05 +0100

    camel, spring, simple route without exception handling

--- src/main/java/com/javainuse/processor/MyProcessor.java
+++ src/main/java/com/javainuse/processor/MyProcessor.java
@@ -1,11 +1,13 @@
 package com.javainuse.processor;
 
+import com.javainuse.exception.CamelCustomException;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
 public class MyProcessor implements Processor {
 
     public void process(Exchange exchange) throws Exception {
-        System.out.println("Processor was called. Hello World from processor");
+        System.out.println("Exception thrown");
+        throw new CamelCustomException();
     }
 }
